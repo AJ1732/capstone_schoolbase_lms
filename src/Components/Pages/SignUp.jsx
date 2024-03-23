0479e730f72ccc2a5e37bd31cbb96fe4bb3cc536
PeaceNaza <peacedonatus38@gmail.com>
2024-03-23 19:28:59 +0100

    feat: Add login event handler

--- src/Components/Pages/SignUp.jsx
+++ src/Components/Pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import Button from "../Layouts/Button"
 import Header from "../Layouts/Header"
 
+
 function SignUp() {
   return (
     <>
@@ -13,11 +14,11 @@ function SignUp() {
 
            <div className="container1">
               <h3 className="mb-2">Name</h3>
-              <input type="text" placeholder="Enter Full Name" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" />
+              <input type="text" placeholder="Enter Full Name" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" required />
               <h3 className="mb-2">Email</h3>
-              <input type="text" placeholder="Email Address" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" />
+              <input type="text" placeholder="Email Address" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" required />
               <h3 className="mb-2">Password</h3>
-              <input type="password" placeholder="Password" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" />
+              <input type="password" placeholder="Password" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" required />
 
               <div className="flex justify-end mb-5 mt-3">
                 <Button type="button" variant="secondary" >SIGNUP</Button>
