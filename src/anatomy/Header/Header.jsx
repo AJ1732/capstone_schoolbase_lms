e424a16f7c27b7b319a0b9d8a07bb645a799a48c
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 00:04:46 +0100

    style: remove header border lines

--- src/anatomy/Header/Header.jsx
+++ src/anatomy/Header/Header.jsx
@@ -3,8 +3,8 @@ import { WhiteButton } from '../../components/Buttons'
 
 const Header = () => {
   return (
-    <header className='content | w-full h-[144px] | text-center border border-black'>
-      <div className="size-full border border-black flex justify-between items-center">
+    <header className='content | w-full h-[144px] | text-center'>
+      <div className="size-full flex justify-between items-center">
         <h1 className="font-generalSans font-semibold text-5xl">Logo</h1>
 
         <div className='font-semibold text-xl | flex justify-center items-center space-x-8'>
