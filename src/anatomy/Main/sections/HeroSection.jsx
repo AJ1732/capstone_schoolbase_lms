422f098448562074b0def72d7c1717f1dcab095d
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 00:15:13 +0100

    chore: build hero section

--- src/anatomy/Main/sections/HeroSection.jsx
+++ src/anatomy/Main/sections/HeroSection.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { HeroForm } from '../../../components/Forms'
+import { TransButton } from '../../../components/Buttons'
 
 const HeroSection = () => {
   return (
-    <section className="content | w-full min-h-[515px]  border border-black flex-center">
-      <h1 className="text-3xl font-bold">HERO SECTION</h1>
+    <section className="full-width content-grid | w-full min-h-[515px] text-white bg-black bg-opacity-80 border border-black">
+      <div className="content | size-full | border border-black flex justify-between items-center">
+        <div className='flex flex-col justify-center items-start gap-5'>
+          <h2 className='font-semibold text-2xl'>Transforming Education with our Versatile Learning Management System</h2>
+          <p className='font-medium text-xl'>Empowering K-12 Education for the Digital Age</p>
+          <div className='space-x-5 mt-2'>
+            <TransButton>Download Now</TransButton>
+            <TransButton>Learn More</TransButton>
+          </div>
+        </div>
+
+        <HeroForm />
+      </div>
     </section>
   )
 }
