f8f9d47f8946e95d896115d3f7859a190d73ae65
AJ1732 <ejemeniboi@gmail.com>
2024-03-23 23:05:30 +0100

    chore: structure the website

--- src/App.jsx
+++ src/App.jsx
@@ -8,74 +8,21 @@ const { features, testimonials, teamMembers } = data;
 function App() {
 
   return (
-    <div className="font-manrope p-10 space-y-10">
-      {/* BUTTONS */}
-      <div>
-        <h2 className="text-2xl font-bold py-3">Buttons</h2>
+    <div className='content-grid | min-h-dvh font-manrope divide-y-[8px] divide-white'>
+      {/* Header */}
+      <header className='full-width content-grid | w-full h-[144px] bg-blacker-gray text-center border border-black'>
+        <div className="content size-full  border border-black"></div>
+      </header>
 
-        <div className="flex flex-col justify-start items-start gap-2">
-          <DarkButton>Subscribe</DarkButton>
-          <LightButton>Subscribe</LightButton>
-          <WhiteButton>Subscribe</WhiteButton>
-          <BorderButton>Subscribe</BorderButton>
-          <TransButton>Subscribe</TransButton>
-        </div>
-      </div>
+      <main className="full-width content-grid | w-full h-fit text-center border border-black ">
+        <div className="content w-full h-[515px]  border border-black"></div>
+        <div className="full-width h-2 bg-white"></div>
+        <div className="breakout w-full h-20  border border-black"></div>
+      </main>
 
-      {/* CARDS */}
-      <div>
-        <h2 className="text-2xl font-bold py-3">Cards</h2>
-
-        {/* Feature Cards */}
-        <h2 className="text-xl font-bold py-3">Feature Card</h2>
-        <div>
-          {features.map( feature => (
-            <FeatureCard 
-              key={feature.id}
-              img={feature.img} 
-              heading={feature.heading} 
-              title={feature.title}
-              content={feature.content}
-            />
-          ))}
-        </div>
-      
-        {/* Testimonial Cards */}
-        <h2 className="text-xl font-bold py-3">Testimonial Card</h2>
-        <div>
-          {testimonials.map( testimonial => (
-            <TestimonialCard 
-              key={testimonial.id}
-              img={testimonial.img} 
-              author={testimonial.author} 
-              content={testimonial.content}
-            />
-          ))}
-        </div>
-
-        {/* Testimonial Cards */}
-        <h2 className="text-xl font-bold py-3">Team Card</h2>
-        <div>
-          {teamMembers.map( teamMember => (
-            <TeamCard 
-              key={teamMember.id}
-              img={teamMember.img} 
-              author={teamMember.author} 
-            />
-          ))}
-        </div>
-      </div>
-
-      {/* FORMS */}
-      <div>
-        <h2 className="text-2xl font-bold py-3">Forms</h2>
-
-        <h2 className="text-xl font-bold py-3">Hero Form</h2>
-        <HeroForm />
-
-        <h2 className="text-xl font-bold py-3">News Form</h2>
-        <NewsForm />
-      </div>
+      <footer className='full-width content-grid | w-full min-h-[290px] bg-blacker-gray text-center border border-black'>
+        <div className="content size-full  border border-black"></div>
+      </footer>
     </div>
   )
 }
