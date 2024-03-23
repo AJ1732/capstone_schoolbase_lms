27c93a76a291a34c7dfc18b7ca06670d6319c42d
AJ1732 <ejemeniboi@gmail.com>
2024-03-23 19:15:49 +0100

    chore: create card components
    created card components for feature and testmonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
 import { BorderButton, DarkButton, LightButton, TransButton, WhiteButton } from "./components/Buttons"
+import { FeatureCard, TestimonialCard } from "./components/Cards"
+import data from './database/data.json'
+
+const { features, testimonials } = data;
+console.log(features[0].img);
+console.log(testimonials);
 
 function App() {
 
   return (
-    <div className="p-10">
-      {/* Buttons */}
-      <div className="flex flex-col justify-start items-start gap-2">
-        <DarkButton>Subscribe</DarkButton>
-        <LightButton>Subscribe</LightButton>
-        <WhiteButton>Subscribe</WhiteButton>
-        <TransButton>Subscribe</TransButton>
-        <BorderButton>Subscribe</BorderButton>
+    <div className="p-10 space-y-10">
+      {/* BUTTONS */}
+      <div>
+        <h2 className="text-2xl font-bold py-3">Buttons</h2>
+        <div className="flex flex-col justify-start items-start gap-2">
+          <DarkButton>Subscribe</DarkButton>
+          <LightButton>Subscribe</LightButton>
+          <WhiteButton>Subscribe</WhiteButton>
+          <TransButton>Subscribe</TransButton>
+          <BorderButton>Subscribe</BorderButton>
+        </div>
+      </div>
+
+      {/* CARDS */}
+      <div>
+        <h2 className="text-2xl font-bold py-3">Cards</h2>
+
+        {/* Feature Cards */}
+        <h2 className="text-xl font-bold py-3">Feature Card</h2>
+        <div>
+          {features.map( feature => (
+            <FeatureCard 
+              key={feature.id}
+              img={feature.img} 
+              heading={feature.heading} 
+              title={feature.title}
+              content={feature.content}
+            />
+          ))}
+        </div>
+      
+        {/* Testimonial Cards */}
+        <h2 className="text-xl font-bold py-3">Testimonial Card</h2>
+        <div>
+          {testimonials.map( testimonial => (
+            <TestimonialCard 
+              key={testimonial.id}
+              img={testimonial.img} 
+              author={testimonial.author} 
+              content={testimonial.content}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
---
+++ src/components/Cards.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import data from '../database/data.json'
+import testimonial_icon from '../assets/testimonial_icon.png'
+// import image from '../assets/feature.png'
+
+export const FeatureCard = ({ img, heading, title, content, className}) => {
+  return (
+    <article className={`max-w-[610px] w-full flex justify-start items-center gap-8 ${className}`}>
+      <figure className='min-w-[300px] h-[225px] bg-dark-gray'>
+        <img src={img} alt="feature image" />
+      </figure>
+      <div className='flex flex-col justify-start items-start gap-3'>
+        <h3 className='font-bold'>{heading}</h3>
+        <p className='text-sm'>
+          <span className='font-semibold'>{title}</span>: {content}
+        </p>
+      </div>
+    </article>
+  )
+}
+
+export const TestimonialCard = ({ img, author, content, className }) => {
+  return (
+    <article className='max-w-[428px] bg-white text-black text-sm p-5 space-y-5'>
+      <figure className='size-6 py-[6px] px-[4px]'>
+        <img src={testimonial_icon} />
+      </figure>
+
+      <p>{content}</p>
+
+      <div className='flex justify-start items-center gap-5'>
+        <figure className='w-[61px] h-[58px]'>
+          <img src={img} alt="author image" />
+        </figure>
+        <div>
+          <h4 className='font-medium text-base'>{author.name}</h4>
+          <p>{author.title}</p>
+        </div>
+      </div>
+    </article>
+  )
+}
