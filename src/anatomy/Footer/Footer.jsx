e6c5b2d8fd1c9bc5697db28b10d7142b91e8725d
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 14:06:26 +0100

    chore: set footer

--- src/anatomy/Footer/Footer.jsx
+++ src/anatomy/Footer/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { NewsForm } from '../../components/Forms'
 
 const Footer = () => {
   return (
     <footer className='full-width content-grid | w-full min-h-[290px] text-center border border-black'>
-      <div className="content | size-full border border-black flex-center">
-        <h1 className="text-3xl font-bold">FOOTER</h1>
+      <div className="content | font-generalSans size-full border border-black flex justify-between items-center">
+        <div>
+          <h4 className='heading text-2xl'>Reach Us</h4>
+          <ul className='list-none flex flex-col justify-start items-start'>
+            <li>
+              <span>+2342 3456 789</span>
+            </li>
+            <li>
+              <span>demo@gmail.com</span>
+            </li>
+            <li>
+              <span>Nigeria</span>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+
+        <NewsForm />
       </div>
     </footer>
   )
