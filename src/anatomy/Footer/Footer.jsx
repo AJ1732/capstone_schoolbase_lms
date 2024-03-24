8187a6919803f6bb2ee34f4bb3542cfda40be000
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 14:32:39 +0100

    feat: build footer section

--- src/anatomy/Footer/Footer.jsx
+++ src/anatomy/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import { NewsForm } from '../../components/Forms'
 
 const Footer = () => {
   return (
-    <footer className='full-width content-grid | w-full min-h-[290px] text-center border border-black'>
-      <div className="content | font-generalSans size-full border border-black flex justify-between items-center">
-        <div>
-          <h4 className='heading text-2xl'>Reach Us</h4>
-          <ul className='list-none flex flex-col justify-start items-start'>
+    <footer className='full-width content-grid | w-full min-h-[290px] text-center'>
+      <div className="content | font-generalSans size-full text-left py-16 flex justify-between items-start">
+        <div className='space-y-5'>
+          <h4 className='heading text-lg'>Reach Us</h4>
+          <ul className='list-none flex flex-col justify-start items-start gap-5'>
             <li>
               <span>+2342 3456 789</span>
             </li>
@@ -20,12 +20,33 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div>
-
+        {/* COMPANY */}
+        <div className='space-y-5'>
+          <h4 className='heading text-lg'>Company</h4>
+          <ul className='list-none flex flex-col justify-start items-start gap-5'>
+            <li>
+              <span>About</span>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+            <li>
+              <span>Blogs</span>
+            </li>
+          </ul>
         </div>
 
-        <div>
-
+        {/* QUICK LINKS */}
+        <div className='space-y-5'>
+          <h4 className='heading text-lg'>Quick Links</h4>
+          <ul className='list-none flex flex-col justify-start items-start gap-5'>
+            <li>
+              <span>Download</span>
+            </li>
+            <li>
+              <span>Forum</span>
+            </li>
+          </ul>
         </div>
 
         <NewsForm />
