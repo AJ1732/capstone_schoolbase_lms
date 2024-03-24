899269b6f4885549ed524ccedbb942323b262f7f
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 11:18:24 +0100

    chore: build testimonial section

--- src/anatomy/Main/sections/TestimonialSection.jsx
+++ src/anatomy/Main/sections/TestimonialSection.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import data from '../../../database/data.json'
+import { TestimonialCard } from '../../../components/Cards'
 
 const TestimonialSection = () => {
+  const { testimonials } = data;
+
   return (
-    <section id='testimonials' className="breakout | w-full min-h-[480px] | bg-black bg-opacity-80 | border border-black flex-center">
-      <h1 className="text-3xl font-bold">TESTIMONIALS</h1>
+    <section id='testimonials' className="full-width content-grid | w-full min-h-[480px] | bg-black bg-opacity-80 | border border-black flex-center">
+      <div className='breakout | py-8 space-y-10'>
+        <h1 className="font-generalSans font-semibold text-2xl text-white">Testimonials</h1>
+
+        <div className='grid grid-cols-3 gap-4'>
+          {testimonials.map( testimonial => (
+            <TestimonialCard 
+              key={testimonial.id}
+              img={testimonial.img} 
+              author={testimonial.author} 
+              content={testimonial.content}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   )
 }
