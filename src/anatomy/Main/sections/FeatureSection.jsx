2778458dc0cfc8ba5a976785dea42dfd1508aef9
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 01:42:22 +0100

    chore: set feature section

--- src/anatomy/Main/sections/FeatureSection.jsx
+++ src/anatomy/Main/sections/FeatureSection.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import data from '../../../database/data.json'
+import { FeatureCard } from '../../../components/Cards'
+
 
 const FeatureSection = () => {
+  const { features, testimonials, teamMembers } = data;
+
   return (
-    <section id='features' className="content | w-full min-h-[1000px]  border border-black flex-center">
-      <h1 className="text-3xl font-bold">FEATURES</h1>
+    <section id='features' className="content | w-full min-h-[1000px] border border-black">
+      <div>
+        <h1 className="text-3xl font-bold">FEATURES</h1>
+        <div className='grid grid-cols-2 gap=x-6 gap-y-12'>
+          {features.map( feature => (
+            <FeatureCard
+              key={feature.id}
+              img={feature.img} 
+              heading={feature.heading} 
+              title={feature.title}
+              content={feature.content}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   )
 }
