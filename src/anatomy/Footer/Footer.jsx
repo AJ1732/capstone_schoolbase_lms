00b8372b81f0dbd10a81ea45ba1e31c76f4941ae
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 12:45:53 +0100

    chore: start footer section

--- src/anatomy/Footer/Footer.jsx
+++ src/anatomy/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='content | w-full min-h-[290px] text-center border border-black'>
-      <div className="size-full border border-black flex-center">
+    <footer className='full-width content-grid | w-full min-h-[290px] text-center border border-black'>
+      <div className="content | size-full border border-black flex-center">
         <h1 className="text-3xl font-bold">FOOTER</h1>
       </div>
     </footer>
