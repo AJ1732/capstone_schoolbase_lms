dcd12f0d2cc833687b4f09b067bf813534deaf20
AJ1732 <ejemeniboi@gmail.com>
2024-03-24 12:09:45 +0100

    chore: build CTA section

--- src/anatomy/Main/sections/CallToSection.jsx
+++ src/anatomy/Main/sections/CallToSection.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { WhiteButton } from '../../../components/Buttons'
 
 const CallToSection = () => {
   return (
-    <section className="full-width content-grid | min-h-[150px] bg-black bg-opacity-80 border border-black">
-      <div className="content | size-full | border border-black flex-center">
-        <h1 className="text-3xl font-bold">CALL TO ACTION</h1>
+    <section className="full-width content-grid | min-h-[150px] bg-black bg-opacity-80">
+      <div className="content | font-generalSans text-white size-full flex justify-between items-center">
+        <div className='flex flex-col items-start justify-center gap-2'>
+          <h4 className='heading'>Subscribe to Our Newsletter</h4>
+          <p className='text-sm'>Subscribe to our Newsletter to receive our blog post, offers, promotions, etc.</p>
+          <WhiteButton className={`text-xs py-[10px] px-10 mt-2`}>Subscribe Now</WhiteButton>
+        </div>
+
+        <div className='w-[313px] h-[75px] bg-light-gray'></div>
       </div>
     </section>
   )
