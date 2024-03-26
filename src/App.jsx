2b832c5560d5bb22c7c389c842360581f9ee0d7d
AJ1732 <ejemeniboi@gmail.com>
2024-03-26 09:49:05 +0100

    chore: update text and spacing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { SignUpForm } from "./components/Form/Forms"
 function App() {
 
   return (
-    <div className="font-manrope p-20">
+    <div className="font-manrope p-20 space-y-5">
 
 
       {/* BUTTONS */}
@@ -21,7 +21,7 @@ function App() {
 
       {/* FORMS */}
       <div>
-        <h2 className="text-2xl font-bold py-3">Buttons</h2>
+        <h2 className="text-2xl font-bold py-3">Forms</h2>
 
         <div className="flex flex-col justify-start items-start gap-2">
           <SignUpForm />
