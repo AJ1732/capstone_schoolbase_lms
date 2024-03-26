587256d3ab9e126bd012107213730aefa111eed8
AJ1732 <ejemeniboi@gmail.com>
2024-03-26 09:52:38 +0100

    feat: create login form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { FormButton, NavButton } from "./components/Button/Buttons"
-import { SignUpForm } from "./components/Form/Forms"
+import { LogInForm, SignUpForm } from "./components/Form/Forms"
 
 function App() {
 
   return (
-    <div className="font-manrope p-20 space-y-5">
+    <div className="font-manrope p-20 space-y-10">
 
 
       {/* BUTTONS */}
       <div>
-        <h2 className="text-2xl font-bold py-3">Buttons</h2>
+        <h2 className="text-3xl font-bold py-3">Buttons</h2>
 
         <div className="flex flex-col justify-start items-start gap-2">
           <NavButton>LOGIN</NavButton>
@@ -21,9 +21,13 @@ function App() {
 
       {/* FORMS */}
       <div>
-        <h2 className="text-2xl font-bold py-3">Forms</h2>
+        <h2 className="text-3xl font-bold py-3">Forms</h2>
 
         <div className="flex flex-col justify-start items-start gap-2">
+          <h2 className="text-xl font-bold py-3">Login Form</h2>
+          <LogInForm />
+
+          <h2 className="text-xl font-bold py-3">Sign Up Form</h2>
           <SignUpForm />
         </div>
       </div>
--- src/components/Form/Forms.jsx
+++ src/components/Form/Forms.jsx
@@ -11,6 +11,135 @@ export const validateEmail = (email) => {
 };
 
 
+export const LogInForm = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+
+  const EmailErrorMessage = () => {
+    return (
+      <p className="field-error">Please enter a valid Email Adresss</p>
+    );
+  };
+
+  const PasswordErrorMessage = () => {
+    return (
+      <p className="field-error">Password should have at least 8 characters</p>
+    );
+  };
+
+  const getIsFormValid = () => { 
+    return ( 
+      fullName && 
+      validateEmail(email) && 
+      password.value.length >= 8 
+    ); 
+  }; 
+  
+  const clearForm = () => {
+    setFullName("");
+    setEmail("");
+    setPassword({
+      value: "",
+      isTouched: false,
+    })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert("Account created!");
+    clearForm();
+  };
+
+  return (
+    <div>
+      <form 
+        action="" 
+        onSubmit={handleSubmit}
+        className={`
+          min-w-[630px] bg-white text-black p-[60px] drop-shadow-lg
+        `}
+      >
+        <fieldset className='flex flex-col gap-5'>
+          {/* NAME */}
+          <div className='field'>
+            <div>
+              <label htmlFor="fullName">Name </label>
+            </div>
+            <input 
+              id='fullName'
+              type="text" 
+              value={fullName}
+              placeholder='Enter Full Name'
+              required
+              onChange={(e) => setFullName(e.target.value)}
+            />
+          </div>
+
+          {/* EMAIL */}
+          <div className='field'>
+            <div>
+              <label htmlFor="email">Email:</label>
+            </div>
+            <input 
+              id='email'
+              type="email" 
+              value={email.value}
+              placeholder='Email Address'
+              onChange={(e) => { 
+                setEmail({ ...email, value: e.target.value }); 
+              }} 
+              onBlur={() => { 
+                setEmail({ ...email, isTouched: true }); 
+              }} 
+            />
+            {
+              email.isTouched && validateEmail(email) == null ? 
+              ( <EmailErrorMessage /> ) :
+              null
+            } 
+          </div>
+
+          {/* PASSWORD */}
+          <div className='field'>
+            <div>
+              <label htmlFor="password">Password </label>
+            </div>
+            <input 
+              id='password'
+              type="password" 
+              value={password.value}
+              placeholder='Password'
+              required
+              onChange={(e) => { 
+                setPassword({ ...password, value: e.target.value }); 
+              }} 
+              onBlur={() => { 
+                setPassword({ ...password, isTouched: true }); 
+              }} 
+            />
+            {
+              password.isTouched && password.value.length < 8 ? 
+              ( <PasswordErrorMessage /> ) :
+              null
+            } 
+          </div>
+          
+          {/* SUBMIT BUTTON */}
+          <FormButton disabled={!getIsFormValid()} className={`w-[150px] mt-[20px] self-end`}>SIGN UP</ FormButton>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
 export const SignUpForm = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState({
