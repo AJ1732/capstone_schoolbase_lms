db656a3b7f954c429e403af0f4b81feaecf14157
AJ1732 <ejemeniboi@gmail.com>
2024-03-26 08:17:03 +0100

    feat: add focus and active states

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -6,6 +6,7 @@ export const NavButton = ({ children, className, onClick }) => {
       onClick={onClick}
       className={`
       min-w-[150px] | text-black text-sm bg-white | py-2.5 px-8 rounded-[10px] 
+      focus:bg-black-gray focus:text-white active:bg-black-gray active:text-white
       ${className}
     `}>
       {children}
