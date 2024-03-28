f857058cb557720d6914b4fde523ee59fff94ed0
AJ1732 <ejemeniboi@gmail.com>
2024-03-28 03:54:07 +0100

    feat: add calender and todo list component

--- src/anatomy/Layouts/Dashboard/Dashboard.jsx
+++ src/anatomy/Layouts/Dashboard/Dashboard.jsx
@@ -3,6 +3,9 @@ import CoursesSection from "./sections/CoursesSection"
 import NotesSection from "./sections/NotesSection"
 import WelcomeSection from "./sections/WelcomeSection"
 
+import { WeekCalender } from '../../../components/Calender/Calender'
+import TodoList from "../../../components/TodoList/TodoList"
+
 function Dashboard() {
   return (
     <div className="grid grid-cols-[280px_1fr_280px] gap-6">
@@ -10,6 +13,10 @@ function Dashboard() {
       <NotesSection />
       <CBTSection />
       <CoursesSection />
+      <div className="grid grid-rows-[8rem_1fr] gap-10">
+        <WeekCalender />
+        <TodoList />
+      </div>
     </div>
   )
 }
