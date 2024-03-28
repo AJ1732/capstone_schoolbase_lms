f9cf1abcd900ef89cf2f6b2cd93041100b153112
AJ1732 <ejemeniboi@gmail.com>
2024-03-29 00:36:11 +0100

    feat: link routes to sidebar nav

--- src/anatomy/Pages/Login.jsx
+++ src/anatomy/Pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-      navigate("/dashboard")
+      navigate("/software")
       console.log(user)
     }).catch((error) => {
       const errorCode = error.code;
