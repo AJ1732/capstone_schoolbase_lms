523d8287e75e73b0fdc0295e8f924957e2dabc1b
AJ1732 <ejemeniboi@gmail.com>
2024-03-30 12:13:48 +0100

    chore: clean unused code

--- src/anatomy/Pages/Login.jsx
+++ src/anatomy/Pages/Login.jsx
@@ -1,56 +1,13 @@
-// import { useState } from "react"
-// import { Button } from '../../Components/Button/Buttons';
-// import { signInWithEmailAndPassword } from "firebase/auth"
-// import { auth } from "../../fireBase"
-
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { LogInForm } from "../../Components/Form/Forms";
 
 const Login = () => {
-
-  // const navigate = useNavigate()
-
-  // const [email, setEmail] = useState("") 
-  // const [password, setPassword] = useState("")
-
-  //function when form is submitted
-  // const submitHandler = (e) => {
-  //   e.preventDefault();
-
-  //   signInWithEmailAndPassword(auth, email, password)
-  //   .then((userCredential) => {
-  //     const user = userCredential.user;
-  //     navigate("/software")
-  //     console.log(user)
-  //   }).catch((error) => {
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     alert(errorCode, errorMessage)
-  //     console.log(errorCode, errorMessage)
-  //   })
-
-  // }
-
   return (
     <div className="content | min-h-[calc(100vh_-_100px)] flex flex-col items-center justify-center">
       <div className="text-center mt-5">
           <h1 className="text-4xl font-normal">WELCOME BACK!</h1>
           <h3>LOGIN HERE</h3>
       </div>
-      {/* <form onSubmit={submitHandler}  >
-        <div className="container1">
-          <h3 className="mb-2">Email</h3>
-          <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" required />
-          <h3 className="mb-2">Password</h3>
-          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" className="w-full px-3 py-2 border border-gray-300 rounded-md mb-3" required />
-          <input type="checkbox" /> <label>Remember me</label>
-
-          <div className="flex justify-end mb-5 mt-3">
-            <Link>Forgot your password? </Link>
-              <Button type="submit" variant="secondary">LOGIN</Button>
-          </div>
-        </div>
-      </form> */}
 
       <LogInForm />
       
