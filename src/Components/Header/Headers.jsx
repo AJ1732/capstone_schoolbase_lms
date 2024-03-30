03b3a0413d7c1aaa7739b4d450416c137d61ce73
PeaceNaza <peacedonatus38@gmail.com>
2024-03-30 11:38:45 +0100

    feat: add icons to header and sidebar

--- src/Components/Header/Headers.jsx
+++ src/Components/Header/Headers.jsx
@@ -4,6 +4,9 @@ import { useValueContext } from '../../context/ContextProvider';
 import { Button } from "../Button/Buttons";
 import { HeaderAvatar } from '../Avatar';
 import avatarImg from '../../assets/avatarImg.png'
+import menu from "../../assets/menu@2x.png"
+import box from "../../assets/box.png"
+import alert from "../../assets/notifications.png"
 
 
 export const HomePageHeader = () => {
@@ -30,7 +33,7 @@ export const SoftwareHeader = () => {
       <div className="content | max-w-[79rem] w-full px-9 py-5 flex items-center justify-between cursor-default">
         
         <button onClick={() => setExpand(prev => !prev)}>
-          <div className='size-6 bg-[#1A1A1A]'></div>
+          <div className='size-6'><img src={menu} alt="menu" /></div>
         </button>
 
         <div className='text-[1A1A1A] ml-32 flex justify-center items-center gap-20'>
@@ -39,8 +42,8 @@ export const SoftwareHeader = () => {
         </div>
 
         <div className='flex justify-center items-center gap-6'>
-          <div className='size-6 bg-[#1A1A1A]'></div>
-          <div className='size-6 bg-[#1A1A1A]'></div>
+          <div className='size-6'><img src={box} alt="" /></div>
+          <div className='size-6'><img src={alert} alt="notifications" /></div>
           <HeaderAvatar src={avatarImg} />
         </div>
       </div>
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useValueContext } from '../../context/ContextProvider'
 import playLessons from "../../assets/play_lesson.png"
+import dashBoard from "../../assets/speed.png"
+import user from "../../assets/person (1).png"
+import notes from "../../assets/news.png"
+import assignment1 from "../../assets/assignment_late.png"
+import assignment2 from "../../assets/assignment_turned_in.png"
+import video from "../../assets/video_chat.png"
+import attendance from "../../assets/event_available.png"
+import calender from "../../assets/calendar_month.png"
+import logout from "../../assets/move_item.png"
 
 
+// eslint-disable-next-line react/prop-types
 const SidebarItem = ({ children, to, text, active, alert }) => {
   const { expand, setExpand } = useValueContext();
   console.log(expand);
@@ -35,59 +45,61 @@ const Sidebar = () => {
         <ul className='flex flex-col justify-center items-start gap-10'>
           {/* DASHBOARD LINK */}
           <SidebarItem to={`/software`} text={`Dashboard`} >
-
+            <img src={dashBoard} alt="speed" />
           </SidebarItem>
 
           {/* USER LINK */}
           <SidebarItem to={`/software/user`} text={`User`}>
-
+           <img src={user} alt="person" />
           </SidebarItem>
 
           {/* LESSONS LINK */}
-          <SidebarItem to={`/software/lessons`} text={`Lesson`}>
+          <SidebarItem to={`/software/lessons`} text={`Lessons`}>
             <img src={playLessons} alt="" />
           </SidebarItem>
 
           {/* NOTES LINK */}
           <SidebarItem to={`/software/notes`} text={`Notes`}>
-
+          <img src={notes} alt="notes" />
           </SidebarItem>
 
           {/* ASSIGNMENT LINK */}
-          <SidebarItem to={`/software/assignments`} text={`Assignment`}>
-
+          <SidebarItem to={`/software/assignments`} text={`Assignments`}>
+            <img src={assignment1} alt="assignment_late" />
           </SidebarItem>
 
           {/* RESULTS LINK */}
-          <SidebarItem to={`/software/results`} text={`Results`}>
-
+          <SidebarItem to={`/software/results`} text={`Check Results`}>
+          <img src={assignment2} alt="assignment_turned_in" />
           </SidebarItem>
           
           {/* CLASSROOM LINK */}
-          <SidebarItem to={`/software/classroom`} text={`Classroom`}>
-
+          <SidebarItem to={`/software/classroom`} text={`Virtual Classroom`}>
+          <img src={video} alt="video_chat_for_classes" />
           </SidebarItem>
           
           {/* ATTENDANCE LINK */}
           <SidebarItem to={`/software/attendance`} text={`Attendance`}>
-
+          <img src={attendance} alt="attendance_for_events" />
           </SidebarItem>
           
           {/* CALENDAR LINK */}
           <SidebarItem to={`/software/calendar`} text={`Calendar`}>
-
+          <img src={calender} alt="calender_month" />
           </SidebarItem>
         </ul>
       </nav>
 
       <div>
         <div onClick={() => setExpand(prev => !prev)} className={`
-          relative | space-x-4 transition-colors cursor-pointer
+          relative | transition-colors cursor-pointer flex items-center justify-center gap-4
         `}>
-          <span className='bg-dark-gray p-2 rounded-full'>0
+          
+          <span className='rounded-full size-6'><img src={logout} alt="logout_your_account" />
           </span>
           <span className={`overflow-hidden ${!expand && ' opacity-0'}`}>Logout</span>
-        </div>
+          </div>
+
       </div>
     </aside>
   )
