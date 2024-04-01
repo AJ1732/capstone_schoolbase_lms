1843fd31eef320b2718941f44f4df9657b788f27
AJ1732 <ejemeniboi@gmail.com>
2024-04-01 01:36:27 +0100

    refactor: remove unused code

--- src/anatomy/Pages/Communication/Communication.jsx
+++ src/anatomy/Pages/Communication/Communication.jsx
@@ -14,24 +14,6 @@ const Communication = () => {
         </figure>
       </div>
 
-      <div className='bg-[#F0F0F0]  rounded-lg flex flex-col '>
-        <div onClick={() => setOpen(!open)} className='w-full flex justify-between items-center py-7 px-5'>
-          <div className='font-semibold flex flex-col gap-5'>
-            <span>Ongoing</span>
-            <span className='text-xl'>1 Subject</span>
-          </div>
-          <span>+</span>
-        </div>
-
-        <div className={`
-          bg-black-gray text-white rounded-b-lg
-          grid overflow-hidden transition-all duration-300 ease-in-out 
-          ${open? 'grid-rows-[1fr] opacity-100 py-7 px-5': 'grid-rows-[0fr] opacity-0'}
-        `}>
-          <p className='overflow-hidden'>This is the answer</p>
-        </div>
-      </div>
-
       <CommAccordion status={`Ongoing`} title={`1 Subject`} content={`This is Content`} />
     </section>
   )
