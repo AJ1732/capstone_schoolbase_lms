5a0f7e5f11e739dcc1308d23eb92fd0170f2454c
AJ1732 <ejemeniboi@gmail.com>
2024-04-01 01:32:45 +0100

    style: update style

--- src/components/Accordion/Accordions.jsx
+++ src/components/Accordion/Accordions.jsx
@@ -4,8 +4,8 @@ export const CommAccordion = ({ title, status, content }) => {
   const [open, setOpen] = useState(false);
 
   return (
-  <div className='bg-[#F0F0F0] text-[#1A1A1A] py-7 px-5 rounded-lg flex flex-col '>
-      <div onClick={() => setOpen(!open)} className='w-full flex justify-between items-center'>
+    <div className='bg-[#F0F0F0] text-[#1A1A1A] rounded-lg flex flex-col '>
+      <div onClick={() => setOpen(!open)} className='w-full flex justify-between items-center py-7 px-5'>
         <div className='font-semibold flex flex-col gap-5'>
           <span>{status}</span>
           <span className='text-xl'>{title}</span>
@@ -14,8 +14,9 @@ export const CommAccordion = ({ title, status, content }) => {
       </div>
 
       <div className={`
+        bg-black-gray text-white rounded-b-lg
         grid overflow-hidden transition-all duration-300 ease-in-out 
-        ${open? 'grid-rows-[1fr] opacity-100 pt-8': 'grid-rows-[0fr] opacity-0'}
+        ${open? 'grid-rows-[1fr] opacity-100 py-7 px-5': 'grid-rows-[0fr] opacity-0'}
       `}>
         <p className='overflow-hidden'>{content}</p>
       </div>
