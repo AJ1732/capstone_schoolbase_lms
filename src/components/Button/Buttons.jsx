cd2fcab2ac96339c0587965ea2b18064f83ab4ea
AJ1732 <ejemeniboi@gmail.com>
2024-04-06 22:08:41 +0100

    chore: update font size of button

--- src/components/Button/Buttons.jsx
+++ src/components/Button/Buttons.jsx
@@ -6,7 +6,7 @@ export const NavButton = ({ children, className, onClick }) => {
     <button 
       onClick={onClick}
       className={`
-      min-w-[150px] | text-black text-sm bg-white | py-2.5 px-8 rounded-[10px] | transition-all duration-300
+      min-w-[150px] | text-black bg-white | py-2.5 px-8 rounded-[10px] | transition-all duration-300
       focus:bg-primary-00 focus:text-white active:bg-primary-00 active:text-white hover:bg-primary-00 hover:text-white
       ${className}
     `}>
@@ -21,7 +21,7 @@ export const FormButton = ({ children, className, onClick, disabled }) => {
       disabled={disabled}
       onClick={onClick}
       className={`
-      min-w-[150px] | text-white text-sm bg-primary-00 | py-2.5 px-8 rounded-[10px] | transition-all duration-300
+      min-w-[150px] | text-white bg-primary-00 | py-2.5 px-8 rounded-[10px] | transition-all duration-300
       active:bg-primary-50 active:text-primary-900
       ${className}
     `}>
