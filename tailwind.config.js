e9779ad9b433e6d91cec02b932cac950c5e56d44
AJ1732 <ejemeniboi@gmail.com>
2024-04-06 17:29:44 +0100

    style: add theme colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,49 @@ export default {
   theme: {
     extend: {
       colors: {
+        'primary-00': '#007BFF',
+        'primary-50': '#F0F2FF',
+        'primary-100': '#E3E8FF',
+        'primary-200': '#CCD6FF',
+        'primary-250': '#D2EDFD',
+        'primary-300': '#89ACFF',
+        'primary-400': '#5F96FF',
+        'primary-500': '#2982FF',
+        'primary-600': '#006EEF',
+        'primary-700': '#004EBA',
+        'primary-800': '#0042A0',
+        'primary-900': '#00337C',
+
+        'secondary-00': '#2C928E',
+
+        'black-00': '#1E1E1E',
+        'black-50': '#F3F3F3',
+        'black-100': '#E9E9E9',
+        'black-200': '#DDDDDD',
+        'black-300': '#B0B0B0',
+        'black-400': '#9B9B9B',
+        'black-500': '#868686',
+        'black-600': '#727272',
+        'black-700': '#505050',
+        'black-800': '#424242',
+        'black-900': '#363636',
+
+        'text-grey': '#676767',
+        'text-black': '#1A1A1A',
+        
+        'blue-150': '#C9E3FF',
+        'pink-150': '#FFCFDB',
+        'amber-150': '#FFEFD2',
+        'green-150': '#E2FFEA',
+
+        'info': '#2F80ED',
+        'error': '#EB5757',
+        'success': '#27AE60',
+        'warning': '#E2B93B',
+
         'dark-gray': '#B2B2B2',
         'text-gray': '#9E9E9E',
         'black-gray': '#414141',
-        'blacker-gray': '#1E1E1E',
         'light-gray': '#E8E8E8',
       },
       fontFamily: {
