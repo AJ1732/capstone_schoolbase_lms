1d6f8b672529c3bd7c3e4b4f225c4217c46645a9
AJ1732 <ejemeniboi@gmail.com>
2024-04-06 23:32:00 +0100

    chore: update styles in cbt section

--- src/anatomy/Pages/Dashboard/sections/CBTSection.jsx
+++ src/anatomy/Pages/Dashboard/sections/CBTSection.jsx
@@ -4,15 +4,15 @@ import { NavButton } from '../../../../components/Button/Buttons'
 const CBTSection = () => {
   return (
     <section className='
-      col-start-2 col-span-2 | bg-[#F0F0F0] size-full rounded-lg | px-9 py-8
+      col-start-2 col-span-2 | bg-white size-full rounded-lg | px-9 py-8 | drop-shadow-md
       flex justify-start items-center
     '>
       <div className='h-full flex flex-col justify-between items-start'>
         <h4 className='font-bold'>Your Last CBT score</h4>
-        <p className='font-semibold text-4xl text-[#1A1A1A]'>76/100</p>
+        <p className='font-semibold text-4xl text-primary-900'>76/100</p>
         <div className='space-x-12'>
-          <NavButton>Retake Test</NavButton>
-          <NavButton>New CBT Test</NavButton>
+          <NavButton className={`text-primary-900 border border-primary-100`}>Retake Test</NavButton>
+          <NavButton className={`!bg-primary-00 text-white`}>New CBT Test</NavButton>
         </div>
       </div>
     </section>
