07b2acc988c53e25de6b4895cbdbb29d7bbeff75
AJ1732 <ejemeniboi@gmail.com>
2024-04-06 22:24:13 +0100

    style: add vertical padding to pages headers

--- src/anatomy/Pages/Login.jsx
+++ src/anatomy/Pages/Login.jsx
@@ -4,7 +4,7 @@ import { LogInForm } from "../../components/Form/Forms";
 const Login = () => {
   return (
     <div className="content | min-h-[calc(100vh_-_100px)] flex flex-col items-center justify-center">
-      <div className="text-center mt-5">
+      <div className="text-center my-5">
           <h1 className="text-4xl font-normal">WELCOME BACK!</h1>
           <h3>LOGIN HERE</h3>
       </div>
--- src/anatomy/Pages/SignUp.jsx
+++ src/anatomy/Pages/SignUp.jsx
@@ -4,7 +4,7 @@ import { SignUpForm } from "../../components/Form/Forms";
 const SignUp = () => {
   return (
     <div className="content | h-[calc(100vh_-_100px)] flex flex-col items-center justify-center mt-12 mb-5">
-      <div className="text-center">
+      <div className="text-center my-5">
         <h1 className="text-4xl font-normal mb-1">CREATE ACCOUNT</h1>
         <h3>SIGN UP HERE</h3>
       </div>
