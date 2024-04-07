19288550fc1ee756ef111d896d05441e2081249b
AJ1732 <ejemeniboi@gmail.com>
2024-04-07 11:07:56 +0100

    chore: update responsiveness

--- src/anatomy/Pages/Login.jsx
+++ src/anatomy/Pages/Login.jsx
@@ -3,18 +3,20 @@ import { LogInForm } from "../../components/Form/Forms";
 
 const Login = () => {
   return (
-    <div className="content | min-h-[calc(100vh_-_100px)] py-10 flex flex-col items-center justify-center">
-      <div className="text-center my-5">
-          <h1 className="text-4xl font-normal">WELCOME BACK!</h1>
-          <h3>LOGIN HERE</h3>
-      </div>
+    <div className="content | min-h-[calc(100vh_-_100px)] py-10">
+      <div className='flex flex-col items-center justify-center gap-5'>
+        <div className="text-center my-5">
+            <h1 className="text-4xl font-normal">WELCOME BACK!</h1>
+            <h3>LOGIN HERE</h3>
+        </div>
 
-      <LogInForm />
-      
-      <h3 className="flex justify-end mr-[_380px] mt-5">
-        Don't have an account?
-        <NavLink to="/signup" className="font-semibold text-primary-00 ml-2">Sign Up</NavLink>
-      </h3>
+        <LogInForm />
+        
+        <h3 className="flex self-end pr-10">
+          Don't have an account?
+          <NavLink to="/signup" className="font-semibold text-primary-00 ml-2">Sign Up</NavLink>
+        </h3>
+      </div>
     </div>
   )
 }
