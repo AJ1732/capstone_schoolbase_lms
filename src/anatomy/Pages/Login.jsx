b904bf46142899a71fff14564528978c08795657
AJ1732 <ejemeniboi@gmail.com>
2024-04-07 09:43:48 +0100

    chore: update spacing in login and signup

--- src/anatomy/Pages/Login.jsx
+++ src/anatomy/Pages/Login.jsx
@@ -3,7 +3,7 @@ import { LogInForm } from "../../components/Form/Forms";
 
 const Login = () => {
   return (
-    <div className="content | min-h-[calc(100vh_-_100px)] flex flex-col items-center justify-center">
+    <div className="content | min-h-[calc(100vh_-_100px)] py-10 flex flex-col items-center justify-center">
       <div className="text-center my-5">
           <h1 className="text-4xl font-normal">WELCOME BACK!</h1>
           <h3>LOGIN HERE</h3>
@@ -13,7 +13,7 @@ const Login = () => {
       
       <h3 className="flex justify-end mr-[_380px] mt-5">
         Don't have an account?
-        <NavLink to="/signup" className="font-semibold ml-2">Sign Up</NavLink>
+        <NavLink to="/signup" className="font-semibold text-primary-00 ml-2">Sign Up</NavLink>
       </h3>
     </div>
   )
--- src/anatomy/Pages/SignUp.jsx
+++ src/anatomy/Pages/SignUp.jsx
@@ -3,13 +3,15 @@ import { SignUpForm } from "../../components/Form/Forms";
 
 const SignUp = () => {
   return (
-    <div className="content | h-[calc(100vh_-_100px)] flex flex-col items-center justify-center mt-12 mb-5">
-      <div className="text-center my-5">
-        <h1 className="text-4xl font-normal mb-1">CREATE ACCOUNT</h1>
-        <h3>SIGN UP HERE</h3>
-      </div>
+    <div className="content | min-h-[calc(100vh_-_100px)]">
+      <div className="py-10 flex flex-col items-center justify-center">
+        <div className="text-center my-5">
+          <h1 className="text-4xl font-normal mb-1">CREATE ACCOUNT</h1>
+          <h3>SIGN UP HERE</h3>
+        </div>
 
-      <SignUpForm />
+        <SignUpForm />
+      </div>
     </div>
   )
 }
