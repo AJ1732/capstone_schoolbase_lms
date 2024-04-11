5f5035cfcd0dc47a99e9c7be7770f84a1519f13d
AJ1732 <ejemeniboi@gmail.com>
2024-04-11 06:53:24 +0100

    feat: add confirm Password to Sign Up Page

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -12,18 +12,6 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   // User State
   const [user, setUser] = useState('');
-  
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      // console.log(currentUser);
-      // console.log(currentUser.uid);
-      // console.log(user);
-    })
-    return () => {
-      unsubscribe();
-    }
-  }, [user]);
 
   // Auth Functions
   const createUser = (email, password) => createUserWithEmailAndPassword(auth, email, password)
@@ -32,6 +20,13 @@ export const AuthProvider = ({ children }) => {
   
   const logOut = () => signOut(auth);
   
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    })
+    return unsubscribe
+  }, []);
+
   // Auth Context Value
   const contextValue = {
     user,
