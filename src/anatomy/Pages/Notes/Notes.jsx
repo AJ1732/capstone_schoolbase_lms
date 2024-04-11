3b55f3b0b769d7a49420d392400e31186168bc9c
PeaceNaza <peacedonatus38@gmail.com>
2024-04-11 11:25:49 +0100

    chore: add assignment to page

--- src/anatomy/Pages/Notes/Notes.jsx
+++ src/anatomy/Pages/Notes/Notes.jsx
@@ -14,14 +14,14 @@ const Notes = () => {
              <div className="flex flex-col  sm:flex-row gap-5 w-auto h-10 mt-1">
               <h3 className="font-bold mt-2">Filter:</h3>
 
-            <div className="flex flex-col gap-2 border-solid shadow-lg px-4 py-2">
+            <div className="flex flex-col gap-2 border-solid shadow rounded-md px-4 py-2">
               <h4>
                 Select Terms
                 <button className="ml-3"><img src={dropdown} alt="" /></button>
               </h4> 
             </div>
 
-            <div className="flex flex-col gap-2 shadow-lg px-4 py-2">
+            <div className="flex flex-col gap-2 shadow rounded-md px-4 py-2">
               <h4>
                 Select Title
                 <button className="ml-3"><img src={dropdown} alt="" /></button>
