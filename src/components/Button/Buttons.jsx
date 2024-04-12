92ed73e1e6a700cf686ea068f94042a3fbbb2243
AJ1732 <ejemeniboi@gmail.com>
2024-04-12 22:59:23 +0100

    feat: implement twMerge into buttons components

--- src/components/Button/Buttons.jsx
+++ src/components/Button/Buttons.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Button.css'
+import { twMerge } from 'tailwind-merge'
 
 export const NavButton = ({ children, className, onClick }) => {
   return (
     <button 
       onClick={onClick}
-      className={`
+      className={twMerge(`
       min-w-[150px] | text-black bg-white | py-2.5 px-8 rounded-[10px] | transition-all duration-300
-      focus:bg-primary-00 focus:text-white active:bg-primary-00 active:text-white hover:bg-primary-00 hover:text-white
-      ${className}
-    `}>
+      focus:bg-primary-00 focus:text-white active:bg-primary-00 active:text-white hover:bg-primary-00 hover:text-white`, 
+      className)}
+    >
       {children}
     </button>
   )
@@ -20,11 +21,11 @@ export const FormButton = ({ children, className, onClick, disabled }) => {
     <button 
       disabled={disabled}
       onClick={onClick}
-      className={`
+      className={twMerge(`
       min-w-[150px] | text-white bg-primary-00 | py-2.5 px-8 rounded-[10px] | transition-all duration-300
-      active:bg-primary-50 active:text-primary-900
-      ${className}
-    `}>
+      active:bg-primary-50 active:text-primary-900`, 
+      className)}
+    >
       {children}
     </button>
   )
