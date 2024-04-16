f182bb94b565ede9152f42e9e2d90387b2f01a30
AJ1732 <ejemeniboi@gmail.com>
2024-04-16 15:20:10 +0100

    refactor: remove unused code

--- src/anatomy/Pages/Dashboard/sections/WelcomeSection.jsx
+++ src/anatomy/Pages/Dashboard/sections/WelcomeSection.jsx
@@ -8,34 +8,17 @@ const WelcomeSection = () => {
   const { user } = useAuthContext();
   
   return (
-    <>
-      <section className='md:col-span-3 | bg-primary-50 py-8 px-5 md:px-12 rounded-md | flex flex-col md:flex-row justify-between items-center gap-10'>
-        <div className="flex justify-start items-start gap-3 md:gap-6">
-          <HeaderAvatar src={avatarImg} />
-          <div className="text-sm md:text-base space-y-2">
-            <h4>Welcome<span className="font-bold">{user? `, ${user.email}` : ''}</span> </h4>
-            <p>Here’s an overview of your classes</p>
-            <p className='text-primary-00'>School Base Solutions</p>
-          </div>
+    <section className='md:col-span-3 | bg-primary-50 py-8 px-5 md:px-12 rounded-md | flex flex-col md:flex-row justify-between items-center gap-10'>
+      <div className="flex justify-start items-start gap-3 md:gap-6">
+        <HeaderAvatar src={avatarImg} />
+        <div className="text-sm md:text-base space-y-2">
+          <h4>Welcome<span className="font-bold">{user? `, ${user.email}` : ''}</span> </h4>
+          <p>Here’s an overview of your classes</p>
+          <p className='text-primary-00'>School Base Solutions</p>
         </div>
-        <img src={dashboardHero} alt="" />
-      </section>
-      
-      {/* <section className="md:col-span-3 | bg-primary-50 w-full h-[14.3125rem] px-5 md:px-12 flex justify-between items-center rounded-lg">
-        <div className="flex justify-start items-start gap-6">
-          <HeaderAvatar src={avatarImg} />
-          <div className="text-sm md:text-base space-y-2">
-            <h4>Welcome<span className="font-bold">{user? `, ${user.email}` : ''}</span> </h4>
-            <p>Here’s an overview of your classes</p>
-            <p className='text-primary-00'>School Base Solutions</p>
-          </div>
-        </div>
-
-        <figure className="max-w-80 max-h-48">
-          <img src={dashboardHero} alt="dashboard img" />
-        </figure>
-      </section> */}
-    </>
+      </div>
+      <img src={dashboardHero} alt="" />
+    </section>
   )
 }
 
