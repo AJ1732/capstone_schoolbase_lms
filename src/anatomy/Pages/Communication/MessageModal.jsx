c6b9702c6344fab006d9f6dbc725b120f4e08711
AJ1732 <ejemeniboi@gmail.com>
2024-04-17 05:18:59 +0100

    feat: adjust transition

--- src/anatomy/Pages/Communication/MessageModal.jsx
+++ src/anatomy/Pages/Communication/MessageModal.jsx
@@ -7,10 +7,13 @@ const MessageModal = ({ open, children, onClose }) => {
   return createPortal(
     <div className={`
       flex justify-center items-center gap-2 ${open? 'top-0': 'top-full'}
-      fixed inset-0 bg-slate-50 bg-opacity-70 | p-10  pt-28 pl-[7.5rem]
-      transition-all duration-500
+      fixed inset-0 bg-slate-50 bg-opacity-70 | p-10 pt-96 pl-[7.5rem]
+      transition-all duration-75 delay-500 overflow-y-scroll
     `}>
-      <div className='relative | bg-primary-200 size-full flex flex-col justify-center items-center rounded-md'>
+      <div className={`
+        relative | max-w-[54rem] size-full bg-primary-200 ${!open? 'h-0 opacity-0': 'h-[43rem] opacity-100'}
+        flex flex-col justify-center items-center rounded-md | transition-all duration-500
+      `}>
         {children}
         <button onClick={onClose} className='absolute top-5 right-5 | bg-primary-100 px-5 py-2 rounded-md'>Close Modal</button>
       </div>
