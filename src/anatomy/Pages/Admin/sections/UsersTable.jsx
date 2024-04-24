b4196c1ef19e01c3bb3ba3695600068421270460
AJ1732 <ejemeniboi@gmail.com>
2024-04-24 15:08:11 +0100

    feat: set users table ui

--- src/anatomy/Pages/Admin/sections/UsersTable.jsx
+++ src/anatomy/Pages/Admin/sections/UsersTable.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UsersTable = () => {
   return (
-    <div className='w-full flex flex-col justify-start items-start gap-10'>
+    <div className='w-full flex flex-col justify-start items-start gap-10 overflow-scroll'>
       <fieldset className='self-end space-x-4'>
         <label htmlFor="sort" className='font-bold text-sm'>Sort by</label>
         <select name="sort" id="sort" className='text-sm p-2 border rounded-md'>
@@ -18,15 +18,15 @@ const UsersTable = () => {
       </fieldset>
 
       {/* TODO: USERS TABLE HERE */}
-      <div className='w-full overflow-y-scroll'>
-        <table class="table-auto w-full">
+      <div className=''>
+        <table class="table-auto min-w-[50rem] w-full overflow-auto">
           <thead className='text-left'>
             <tr>
-              <th>Song</th>
-              <th>Artist</th>
-              <th>Year</th>
-              <th>Year</th>
-              <th>Year</th>
+              <th>User</th>
+              <th>Role</th>
+              <th>Date Added</th>
+              <th>Status</th>
+              <th>Edit</th>
             </tr>
           </thead>
           <tbody>
